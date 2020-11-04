d8f3fdac65bad8a1270bd1c10f0b78d11ba243ac
rgiri1706 <rgiri1706@gmail.com>
2020-11-04 12:58:33 +0530

    Stopwatch made in react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    let [count,setCount] = useState(0);
+    let [isClicked, toggleClicked] = useState(false);
+    let [counterId, setId] = useState(null);
+
+    useEffect(()=>{
+      if(isClicked){
+        let id=setTimeout(()=>{
+          setCount(count+1)
+        },1000);
+        setId(id);
+      }
+    },[isClicked, count])
+
+    const handleClick=()=>{
+      toggleClicked(!isClicked);
+    }
+
+    const stopTimer=()=>{
+      toggleClicked(false);
+      setCount(0);
+      clearInterval(counterId);
+    }
+
+    return (
+      <div className="App">
+          <div>
+            {count}
+          </div>
+          {isClicked ? <button onClick={handleClick}>
+            pause
+          </button> : <button onClick={handleClick}>
+            Start
+          </button>}
+          <button onClick={stopTimer}>
+            Stop
+          </button>
+      </div>
+    );
+}
+
+export default App;
